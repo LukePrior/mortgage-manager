717399b2dd76d4d94141d56dab319237a2a4eb1e
Uskompuf <22492406+Uskompuf@users.noreply.github.com>
2022-06-28 17:38:36 +1000

    dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { productData, rateData } from "./interfaces";
-import { Column } from "@material-table/core";
+import MaterialTable, { Column } from "@material-table/core";
 import Detailed from "./detailed";
 import MenuItem from "@mui/material/MenuItem";
 import ListItemText from "@mui/material/ListItemText";
@@ -14,34 +14,48 @@ import OutlinedInput from "@mui/material/OutlinedInput";
 import Grid from "@mui/material/Grid";
 import InputAdornment from "@mui/material/InputAdornment";
 import InputLabel from "@mui/material/InputLabel";
-
-const MaterialTable = require("@material-table/core").default;
-
-const darkTheme = createTheme({
-  palette: {
-    mode: "light"
-  }
-});
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Paper from "@mui/material/Paper";
+import Container from "@mui/material/Container";
+import IconButton from "@mui/material/IconButton";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
 
 export default function App() {
   const [store, setStore] = useState([]);
   const [data, setData] = useState([]);
-  const [loanPeriod, setLoanPeriod] = React.useState<string | null>(null);
-  const [loanPayment, setLoanPayment] = React.useState<string | null>(null);
-  const [loanBig, setLoanBig] = React.useState<string | null>(null);
+  const [loanPeriod, setLoanPeriod] = React.useState<string | null>("0");
+  const [loanPayment, setLoanPayment] = React.useState<string | null>("0");
+  const [loanBig, setLoanBig] = React.useState<string | null>("0");
   const [value, setValue] = React.useState<number | string | null>(600000);
+  const savedDarkMode = window.localStorage.getItem("darkState");
+  const defaultDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const prefersDarkMode = savedDarkMode || defaultDarkMode;
+  const [darkMode, setDarkMode] = React.useState(prefersDarkMode);
+
+  const defaultTheme = createTheme({
+    palette: {
+      mode: darkMode ? "dark" : "light"
+    }
+  });
 
   function sortTable(
     loanPeriod: string | null,
     loanPayment: string | null,
-    loanBig: string | null
+    loanBig: string | null,
+    allData?: any
   ) {
+    let localStore = store;
+    if (allData !== undefined) {
+      localStore = allData;
+    }
+
     if (loanPeriod === null && loanPayment === null && loanBig === null) {
-      setData(store);
+      setData(localStore);
       return;
     }
 
-    let newData = store.filter((product: any) => {
+    let newData = localStore.filter((product: any) => {
       let flag = false;
       product.rate.forEach((rate: rateData, index: number) => {
         if (
@@ -108,6 +122,8 @@ export default function App() {
     {
       title: "Rate",
       field: "rate[0].rate",
+      customFilterAndSearch: (term, rowData) =>
+        term === rowData.rate[rowData.i].rate + "%",
       customSort: (a, b) => a.rate[a.i].rate - b.rate[b.i].rate,
       render: (rowData) => {
         const handleChange = (event: SelectChangeEvent) => {
@@ -171,9 +187,14 @@ export default function App() {
       render: (rowData) => {
         let i = rowData.rate[rowData.i].rate / 12 / 100;
         let n = 300;
-        let p = value;
+        let p;
+        if (value === null || typeof value === "string") {
+          p = 0;
+        } else {
+          p = value;
+        }
         let r = Math.round((p * (i * (1 + i) ** n)) / ((1 + i) ** n - 1));
-        return <p>${r}</p>;
+        return <p>${r.toLocaleString()}</p>;
       },
       customSort: (a, b) => {
         return a.rate[a.i].rate - b.rate[b.i].rate;
@@ -193,12 +214,53 @@ export default function App() {
         }
         return <p>{rowData.rate[rowData.i].lendingRateType}</p>;
       }
+    },
+    {
+      title: "Repayment",
+      field: "rate[0].repaymentType",
+      hidden: true,
+      customSort: (a, b) => {
+        if (!a.rate[a.i].hasOwnProperty("repaymentType"))
+          a.rate[a.i].repaymentType = "";
+        if (!b.rate[b.i].hasOwnProperty("repaymentType"))
+          b.rate[b.i].repaymentType = "";
+        return a.rate[a.i].repaymentType.localeCompare(
+          b.rate[b.i].repaymentType
+        );
+      },
+      render: (rowData) => {
+        return <p>{rowData.rate[rowData.i].repaymentType}</p>;
+      }
     }
   ];
+
+  const options = {
+    columnsButton: true
+  };
 
   const detailPanel = (rowData: { rowData: productData }) => {
     const data = rowData.rowData;
     return <Detailed data={data} />;
+  };
+
+  useEffect(() => {
+    const existingPreference = localStorage.getItem("darkState");
+    if (existingPreference === "true") {
+      setDarkMode(true);
+    } else if (existingPreference === "false") {
+      setDarkMode(false);
+    } else {
+      setDarkMode(prefersDarkMode);
+    }
+  }, [prefersDarkMode]);
+
+  const handleDarkModeToggle = () => {
+    setDarkMode(!darkMode);
+    if (darkMode) {
+      localStorage.setItem("darkState", "false");
+    } else {
+      localStorage.setItem("darkState", "true");
+    }
   };
 
   useEffect(() => {
@@ -210,74 +272,113 @@ export default function App() {
         actualData.forEach((o: any, i: number, a: any) => {
           a[i].i = 0;
         });
-        setData(actualData);
         setStore(actualData);
+        sortTable(loanPeriod, loanPayment, loanBig, actualData);
       });
   }, []);
 
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
-      <Grid container justifyContent="center" spacing={1} sx={{ my: 1 }}>
-        <Grid item style={{ display: "flex" }}>
-          <ToggleButtonGroup
-            value={loanPeriod}
-            exclusive
-            onChange={handleLoanPeriod}
-            color="primary"
-          >
-            <ToggleButton value="0">Variable</ToggleButton>
-            <ToggleButton value="1">1 YR</ToggleButton>
-            <ToggleButton value="2">2 YR</ToggleButton>
-            <ToggleButton value="3">3 YR</ToggleButton>
-            <ToggleButton value="4">4 YR</ToggleButton>
-            <ToggleButton value="5">5 YR</ToggleButton>
-            <ToggleButton value="10">10 YR</ToggleButton>
-          </ToggleButtonGroup>
+      <Container maxWidth="xl" sx={{ p: 0.5 }}>
+        <Paper elevation={1} sx={{ p: 1, mb: 1, mt: 1 }}>
+          <h1>Mortgage Manager</h1>
+          <p>
+            A tool to compare home loans from a wide range of Australian lenders
+            to help find the perfect mortgage to suit your needs.
+          </p>
+        </Paper>
+        <Paper elevation={1} sx={{ p: 1 }}>
+          <Grid container justifyContent="center" spacing={1} sx={{ p: 1 }}>
+            <Grid item style={{ display: "flex" }}>
+              <ToggleButtonGroup
+                value={loanPeriod}
+                exclusive
+                onChange={handleLoanPeriod}
+                color="primary"
+              >
+                <ToggleButton value="0">Variable</ToggleButton>
+                <ToggleButton value="1">1 YR</ToggleButton>
+                <ToggleButton value="2">2 YR</ToggleButton>
+                <ToggleButton value="3">3 YR</ToggleButton>
+                <ToggleButton value="4">4 YR</ToggleButton>
+                <ToggleButton value="5">5 YR</ToggleButton>
+                <ToggleButton value="10">10 YR</ToggleButton>
+              </ToggleButtonGroup>
+            </Grid>
+            <Grid item style={{ display: "flex" }}>
+              <ToggleButtonGroup
+                value={loanPayment}
+                exclusive
+                onChange={handleLoanPayment}
+                color="primary"
+              >
+                <ToggleButton value="0">Principle & Interest</ToggleButton>
+                <ToggleButton value="1">Interest Only</ToggleButton>
+              </ToggleButtonGroup>
+            </Grid>
+            <Grid item style={{ display: "flex" }}>
+              <ToggleButtonGroup
+                value={loanBig}
+                exclusive
+                onChange={handleLoanBig}
+                color="primary"
+              >
+                <ToggleButton value="0">BIG 4</ToggleButton>
+              </ToggleButtonGroup>
+            </Grid>
+            <Grid item style={{ display: "flex" }}>
+              <FormControl>
+                <InputLabel htmlFor="outlined-adornment-amount">
+                  Amount
+                </InputLabel>
+                <OutlinedInput
+                  type="number"
+                  id="outlined-adornment-amount"
+                  startAdornment={
+                    <InputAdornment position="start">$</InputAdornment>
+                  }
+                  value={value}
+                  onChange={(e) => setValue(e.target.value)}
+                  label="Amount"
+                />
+              </FormControl>
+            </Grid>
+          </Grid>
+        </Paper>
+        <Grid sx={{ mt: 1 }}>
+          <MaterialTable
+            columns={columns}
+            data={data}
+            detailPanel={detailPanel}
+            options={options}
+            title="Home Loans"
+          />
         </Grid>
-        <Grid item style={{ display: "flex" }}>
-          <ToggleButtonGroup
-            value={loanPayment}
-            exclusive
-            onChange={handleLoanPayment}
-            color="primary"
-          >
-            <ToggleButton value="0">Principle & Interest</ToggleButton>
-            <ToggleButton value="1">Interest Only</ToggleButton>
-          </ToggleButtonGroup>
-        </Grid>
-        <Grid item style={{ display: "flex" }}>
-          <ToggleButtonGroup
-            value={loanBig}
-            exclusive
-            onChange={handleLoanBig}
-            color="primary"
-          >
-            <ToggleButton value="0">BIG 4</ToggleButton>
-          </ToggleButtonGroup>
-        </Grid>
-        <Grid item style={{ display: "flex" }}>
-          <FormControl>
-            <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
-            <OutlinedInput
-              type="number"
-              id="outlined-adornment-amount"
-              startAdornment={
-                <InputAdornment position="start">$</InputAdornment>
-              }
-              value={value}
-              onChange={(e) => setValue(e.target.value)}
-              label="Amount"
-            />
-          </FormControl>
-        </Grid>
-      </Grid>
-      <MaterialTable
-        columns={columns}
-        data={data}
-        detailPanel={detailPanel}
-        title="Home Loans"
-      />
+        <Paper elevation={1} sx={{ p: 1, mt: 1 }}>
+          <Grid container>
+            <Grid container item xs={10} justifyContent="flex-start">
+              <p>
+                Mortgage Manager was created by Luke Prior for the CSESoc x
+                Pearler Competition.
+              </p>
+            </Grid>
+            <Grid container item xs={2} justifyContent="flex-end">
+              <IconButton
+                sx={{ ml: 1 }}
+                onClick={handleDarkModeToggle}
+                color="inherit"
+              >
+                {defaultTheme.palette.mode === "dark" ? (
+                  <Brightness7Icon />
+                ) : (
+                  <Brightness4Icon />
+                )}
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Container>
     </ThemeProvider>
   );
 }
